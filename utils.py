808242b8e412d4d4bbcd5262d5233818a49d540c
Tydus <Tydus@Tydus.org>
2022-12-01 02:11:15 +0900

    Fix traci load

--- utils.py
+++ utils.py
@@ -8,6 +8,12 @@ import math
 import random
 from typing import *
 
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
 import traci
 import dataclasses
 import numpy as np
